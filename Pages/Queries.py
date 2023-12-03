b95539b16e7b4fb3d6a84383578e25acbb89ace4
Shanmugagouthaman <65980015+SG9822@users.noreply.github.com>
2023-12-03 22:35:42 +0530

    Add files via upload

---
+++ Pages/Queries.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from Inserting_to_MySQL import engine
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Query Details",
+    page_icon="📊",
+)
+
+st.sidebar.success("Select a page above")
+
+query_dict = {
+    'What are the names of all the videos and their corresponding channels?': [
+        'select video_name, Channel_Name from channel c left join video v on c.Channel_Id = v.Channel_Id'],
+    'Which channels have the most number of videos, and how many videos do they have?': [
+        'select Channel_Name, Video_Count from channel order by Video_Count desc limit 1', 'Channel_Name',
+        'Video_Count'],
+    'What are the top 10 most viewed videos and their respective channels?': [
+        'select Video_Name, Channel_Name, View_Count from channel c left join video v  on c.Channel_Id = v.Channel_Id order by View_Count desc limit 10',
+        'Video_Name', 'View_Count'],
+    'How many comments were made on each video, and what are their corresponding video names?': [
+        'select Video_Name, Comment_Count from video', 'Video_Name', 'Comment_Count'],
+    'Which videos have the highest number of likes, and what are their corresponding channel names?': [
+        'select Channel_Name, Video_Name, Like_Count from channel c join video v on c.Channel_Id = v.Channel_Id order by Like_Count desc',
+        'Video_Name', 'Like_Count'],
+    'What is the total number of likes for each video, and what are their corresponding video names?': [
+        'select Video_Name, Like_Count from video order by Like_Count desc', 'Video_Name', 'Like_Count'],
+    'What is the total number of views for each channel, and what are their corresponding channel names?': [
+        'select Channel_Name, Channel_Views from channel', 'Channel_Name', 'Channel_Views'],
+    'What is the average duration of all videos in each channel, and what are their corresponding channel names?': [
+        'select Channel_Name, round(avg(Duration), 3) Average_Duration  from channel c left join video v on c.Channel_Id = v.Channel_Id group by Channel_Name',
+        'Channel_Name', 'Average_Duration'],
+    'What are the names of all the channels that have published videos in the year 2022?': [
+        'select Distinct Channel_Name, year(Published_Date) year_published from channel c join video v on c.Channel_Id = v.Channel_Id where year(Published_Date) = 2022'],
+    'Which videos have the highest number of comments, and what are their corresponding channel names ?': [
+        'select Channel_Name, Video_Name, Comment_Count from channel c join video v on c.Channel_Id = v.Channel_Id order by Comment_Count desc limit 5',
+        'Video_Name', 'Comment_Count']
+}
+
+
+def query(query_dict, s):
+    if len(query_dict[s]) > 1:
+        table = pd.read_sql_query(query_dict[s][0], engine)
+        table = pd.DataFrame(table)
+        st.dataframe(table)
+        st.header("Chart")
+        st.write(px.bar(table, x=query_dict[s][1], y=query_dict[s][2]))
+
+    else:
+        table = pd.read_sql_query(query_dict[s][0], engine)
+        table = pd.DataFrame(table)
+        if s == 'What are the names of all the channels that have published videos in the year 2022?':
+            table["year_published"] = pd.to_datetime(table["year_published"], format="%Y").dt.strftime("%Y")
+        st.write(table)
+        st.caption("Sorry Chart can't be formed")
+
+
+
+st.image("Statistics-Vector-PNG-Pic.png", width=150)
+st.title("Query Details")
+st.header("Want to know some Stats and Ranks?")
+
+select = st.selectbox("Select query", list(query_dict.keys()), index=None)
+q = st.button("Query")
+if q:
+    # print(query(query_dict, select))
+    try:
+        print(query(query_dict, select))
+
+    except:
+        st.warning("Please Provide the Query")
---
+++ Pages/Tables.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+from Inserting_to_MySQL import engine
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Table Details",
+    page_icon="📚",
+)
+
+st.sidebar.success("Select a page above")
+
+st.image("How-to-Join-Two-Tables-in-SQL.png", width=100)
+
+
+st.title("Want to see Tables stored in our Database that you are working with?")
+table = st.selectbox("Select the table you want to explore", ["Channels", "Videos", "Comments"], index=None)
+
+query = ""
+if table:
+    try:
+        if table == 'Channels':
+            query = "select * from channel"
+        elif table == 'Videos':
+            query = "select * from video"
+        elif table == 'Comments':
+            query = "select * from comments"
+
+        sql = pd.read_sql_query(query, engine)
+        st.write(pd.DataFrame(sql))
+
+    except:
+        st.warning("No Tables in the Database")
+
+st.header("Top 10 Stars")
+st.subheader("Select both radio and select option for Top 10 stars")
+
+top_radio = st.radio("Select the table you want to explore", ["Channel", "Video"], index=None)
+
+list = []
+if top_radio == "Channel":
+    list = ["Subscription_Count", "Video_Count", "Channel_Views"]
+else:
+    list = ["View_Count", "Like_Count", "Comment_Count"]
+
+top_select = st.selectbox("Select Sub query", list, index=None)
+
+if top_radio and top_select:
+    quotes = f"select {top_radio}_name, {top_select} from {top_radio} order by {top_select} desc limit 10"
+    sql_table = pd.read_sql_query(quotes, engine)
+    st.write(pd.DataFrame(sql_table))
+    st.write(px.bar(sql_table, x=f"{top_radio}_name", y=top_select))

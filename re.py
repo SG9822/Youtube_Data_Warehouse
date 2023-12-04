b0123edf417c8e7f94f878c3fc9a765c97e20cc8
Shanmugagouthaman <65980015+SG9822@users.noreply.github.com>
2023-12-04 08:30:04 +0530

    Delete re.py

--- re.py
+++
@@ -1,9 +0,0 @@
-print("Christmas Songs 2024 ".strip())
-# import re
-#
-# list = ["Vj Siddhu Vlogs", "Christmas Songs 2024 ", "LooLoo Kids - Nursery Rhymes and Children's Songs"]
-#
-# for i in list:
-#     print(str(re.sub(' +', ' ', i)))
-
-
